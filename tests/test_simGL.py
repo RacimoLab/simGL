eda4a16f404b46b83b671ea5001d4e7a4d816773
MoiColl <moisescollmacia@gmail.com>
2022-02-07 13:36:43 +0100

    extending testing on sim_allelereadcounts() function

--- tests/test_simGL.py
+++ tests/test_simGL.py
@@ -1,7 +1,7 @@
 import msprime
 import simGL
 import tskit
-import numpy
+import numpy as np
 
 
 def sim_ts(seeds=(1, 2), n=5):
@@ -25,6 +25,41 @@ def test_sim_allelereadcounts():
     arc = simGL.sim_allelereadcounts(gm = ts.genotype_matrix(), mean_depth = 15, std_depth = 2.5, e = 0.05, ploidy = 2, seed = 1234)
     assert arc.shape == (ts.num_sites, num_individuals, 4)
 
+def test_allelereadcounts_to_GL_inputs():
+    ts = sim_ts()
+    ref = np.array([v.site.ancestral_state for v in ts.variants()])
+    alt = np.array([v.site.mutations[0].derived_state for v in ts.variants()])
+    # gm is :
+    #     - not a numpy array
+    #     - is a numpy array with only one dimention
+    #     - is a numpy array with values different than 1 or 0
+    for gm in [2, np.array([0, 1, 1, 0]), np.array([[0, 3], [1, 0]])]:
+        try:
+            simGL.sim_allelereadcounts(gm = gm, mean_depth = 30, e = 0.05, ploidy = 2, seed = 1234, std_depth = 5, ref = ref, alt = alt)
+        except TypeError:
+            pass
+    gm = ts.genotype_matrix()
+    # mean_depth is:
+    #     - string
+    #     - negative value
+    #     - numpy array with more than 1 dimention
+    #     - numpy array with 1 dimention and negative or 0 values
+    #     - numpy array with 1 dimention and positive values with a different size than gm.shape[1]
+    for mean_depth in ["30", -1, np.array([[15], [15]]), np.array([15, 0]), np.full(gm.shape[1]-1, 15)]:
+        try:
+            simGL.sim_allelereadcounts(gm = gm, mean_depth = mean_depth, e = 0.05, ploidy = 2, seed = 1234, std_depth = 5, ref = ref, alt = alt)
+        except TypeError:
+            pass
+    # std_depth is:
+    # - string
+    # - negative number
+    # - numpy array
+    for std_depth in ["5", -4, np.array([3])]:
+        try:
+            print(isinstance(std_depth, (int, float)))
+            simGL.sim_allelereadcounts(gm = gm, mean_depth = 30, e = 0.05, ploidy = 2, seed = 1234, std_depth = std_depth, ref = ref, alt = alt)
+        except TypeError:
+            pass
 
 def test_allelereadcounts_to_GL():
     # TODO: proper tests
